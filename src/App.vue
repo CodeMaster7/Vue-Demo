d179835ef5efd74fea72cc5d2a8fc62588a24aec
SethLee <sethlimlee@gmail.com>
2018-04-26 12:11:17 -0600

    2nd commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>My Blogging App!</h1>
+    <button @click='toggle(false)'>New Post</button>
+    <button @click='toggle(true)'>Show All Posts</button>
+    <section v-show='!showPosts'>
+      <form>
+        <h4>Create New Post:</h4>
+        <textarea name="" id="" cols="90" rows="7"></textarea>
+        <br>
+        <button type='submit'>Add New Post</button>
+      </form>
+    </section>
+    <section v-show='showPosts'>
+    <h4>All Posts:</h4>
+      <div v-for='(post,i) in posts' :key='i'>
+        <p>Data: {{post.date}}</p>
+        <p>Message: {{post.message}}</p>
+        <hr>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPosts: true,
+      posts: [{ date: new Date(), message: "test post" }]
+    };
+  },
+  methods: {
+    toggle(bool) {
+      this.showPosts = bool
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
